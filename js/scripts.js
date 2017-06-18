90b5c5a3ef975c81579c7983d5811d59b29f0ea7
Calvin Cushman <calvincushman@gmail.com>
2017-06-18 11:22:00 -0700

    add click function for art pop up on main div

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
 $(function(){
+  $("#box1").click(function(){
+    event.preventDefault();
+    $(".art").show(30);
+    $(".contact").hide();
+    $(".programming").hide();
+    $(".about").hide();
+    $(".top").hide(6000);
+  });
   $("#box3").click(function(){
     event.preventDefault();
     $(".contact").show(30);
+    $(".programming").hide();
+    $(".art").hide();
+    $(".about").hide();
+    $(".top").hide(6000);
+  });
+  $("#box4").click(function(){
+    event.preventDefault();
+    $(".programming").show(30);
+    $(".contact").hide();
+    $(".about").hide();
+    $(".art").hide();
     $(".top").hide(6000);
   });
 });
